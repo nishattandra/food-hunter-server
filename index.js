0fe180d151bcc740c1b59bbbdcafd30945994ea8
nishattandra <nishattandra2001@gmail.com>
2023-05-04 00:43:13 +0600

    initialization done

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+const chefs = require('./data/chef.json')
+
+app.get('/', (req, res) => {
+    res.send('chef data is loading')
+});
+
+app.get('/chefs', (req, res) => {
+    res.send(chefs);
+})
+
+app.get('/chefs/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = chefs.find(n => n.id == id);
+    res.send(selectedNews)
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Recipe API is running on port: ${port}`)
+});
